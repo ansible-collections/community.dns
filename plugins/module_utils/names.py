d7c07f6ae808a014c9f5d133e7e802ef8eb0b5e8
Felix Fontein <felix@fontein.de>
2021-04-25 09:52:44 +0200

    Move DNS name handling code to module_utils.

---
+++ plugins/module_utils/names.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 Felix Fontein
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import re
+
+from ansible.module_utils._text import to_text
+
+
+_ONLY_ALABELS_MATCHER = re.compile(r'^[a-zA-Z0-9.-]*$')
+
+
+def only_alabels(domain):
+    '''
+    Check whether domain name has only alabels.
+    '''
+    return _ONLY_ALABELS_MATCHER.match(domain) is not None
+
+
+class InvalidDomainName(Exception):
+    '''
+    The provided domain name is not valid.
+    '''
+    pass
+
+
+def split_into_labels(domain):
+    '''
+    Split domain name to a list of labels. Start with the top-most label.
+
+    Returns a list of labels and a tail, which is either ``''`` or ``'.'``.
+    Raises ``InvalidDomainName`` if the domain name is not valid.
+    '''
+    result = []
+    index = len(domain)
+    tail = ''
+    if domain.endswith('.'):
+        index -= 1
+        tail = '.'
+    if index > 0:
+        while index >= 0:
+            next_index = domain.rfind('.', 0, index)
+            label = domain[next_index + 1:index]
+            if label == '' or label[0] == '-' or label[-1] == '-' or len(label) > 63:
+                raise InvalidDomainName(domain)
+            result.append(label)
+            index = next_index
+    return result, tail
+
+
+def normalize_label(label):
+    '''
+    Normalize a domain label. Returns a lower-case alabel.
+    '''
+    if label not in ('', '*') and not only_alabels(label):
+        # Convert ulabel to alabel
+        label = to_text(b'xn--' + to_text(label).encode('punycode'))
+    # Always convert to lower-case
+    return label.lower()
---
+++ tests/unit/plugins/module_utils/test_names.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# (c) 2021, Felix Fontein <felix@fontein.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Make coding more python3-ish
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+import pytest
+
+from ansible_collections.community.dns.plugins.module_utils.names import (
+    only_alabels,
+    normalize_label,
+    split_into_labels,
+    InvalidDomainName,
+)
+
+
+TEST_ONLY_ALABELS = [
+    ('asdf', True),
+    ('', True),
+    ('ä', False),
+    ('☹', False),
+]
+
+
+@pytest.mark.parametrize("domain, result", TEST_ONLY_ALABELS)
+def test_only_alabels(domain, result):
+    assert only_alabels(domain) == result
+
+
+TEST_LABEL_SPLIT = [
+    ('', [], ''),
+    ('.', [], '.'),
+    ('com', ['com'], ''),
+    ('com.', ['com'], '.'),
+    ('foo.bar', ['bar', 'foo'], ''),
+    ('foo.bar.', ['bar', 'foo'], '.'),
+    ('*.bar.', ['bar', '*'], '.'),
+    (u'☺.A', ['A', u'☺'], ''),
+]
+
+
+@pytest.mark.parametrize("domain, labels, tail", TEST_LABEL_SPLIT)
+def test_split_into_labels(domain, labels, tail):
+    _labels, _tail = split_into_labels(domain)
+    assert _labels == labels
+    assert _tail == tail
+
+
+TEST_LABEL_SPLIT_ERRORS = [
+    '.bar.',
+    '..bar',
+    '-bar',
+    'bar-',
+]
+
+
+@pytest.mark.parametrize("domain", TEST_LABEL_SPLIT_ERRORS)
+def test_split_into_labels_errors(domain):
+    with pytest.raises(InvalidDomainName):
+        split_into_labels(domain)
+
+
+TEST_LABEL_NORMALIZE = [
+    ('', ''),
+    ('*', '*'),
+    ('foo', 'foo'),
+    ('Foo', 'foo'),
+    (u'hëllö', 'xn--hll-jma1d'),
+    (u'食狮', 'xn--85x722f'),
+    (u'☺', 'xn--74h'),
+    (u'😉', 'xn--n28h'),
+]
+
+
+@pytest.mark.parametrize("label, normalized_label", TEST_LABEL_NORMALIZE)
+def test_normalize_label(label, normalized_label):
+    print(normalize_label(label))
+    assert normalize_label(label) == normalized_label
