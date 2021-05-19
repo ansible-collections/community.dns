93a3d7ece0a6d20830fc3b1450717ae31e977b58
Felix Fontein <felix@fontein.de>
2021-05-19 09:10:29 +0200

    Linting.

--- update-psl.sh
+++ update-psl.sh
@@ -8,9 +8,8 @@ git status plugins/public_suffix_list.dat
 if [ -n "$(git status --porcelain=v1 plugins/public_suffix_list.dat)" ]; then
     git diff
     if [ ! -e changelogs/fragments/update-psl.yml ]; then
-        echo x
         echo "bugfixes:" > changelogs/fragments/update-psl.yml
-        echo '- "Update Public Suffix List."' >> changelogs/fragments/update-psl.yml
+        echo '  - "Update Public Suffix List."' >> changelogs/fragments/update-psl.yml
     fi
     exit 1
 fi
