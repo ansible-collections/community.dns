81aa38978fc12dd01c9eb5f42cf867a67eed3760
Felix Fontein <felix@fontein.de>
2021-07-13 22:16:08 +0200

    Linting.

--- plugins/inventory/hosttech_dns_records.py
+++ plugins/inventory/hosttech_dns_records.py
@@ -4,6 +4,9 @@
 # Copyright (c) 2020 Markus Bergholz <markuman+spambelongstogoogle@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 DOCUMENTATION = '''
 name: hosttech_dns_records
 plugin_type: inventory
