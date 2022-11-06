ac6783cd2d3985b9c3f1e4345d42ec30bc6e8b3f
Felix Fontein <felix@fontein.de>
2022-11-06 12:29:33 +0100

    Compatibility with older ansible-core releases.

--- plugins/doc_fragments/attributes.py
+++ plugins/doc_fragments/attributes.py
@@ -12,6 +12,7 @@ class ModuleDocFragment(object):
 
     # Standard documentation fragment
     DOCUMENTATION = r'''
+options: {}
 attributes:
     check_mode:
       description: Can run in C(check_mode) and return changed status prediction without modifying target.
@@ -23,6 +24,7 @@ attributes:
 '''
 
     ACTIONGROUP_HETZNER = r'''
+options: {}
 attributes:
     action_group:
       description: Use C(group/community.dns.hetzner) in C(module_defaults) to set defaults for this module.
@@ -32,6 +34,7 @@ attributes:
 '''
 
     ACTIONGROUP_HOSTTECH = r'''
+options: {}
 attributes:
     action_group:
       description: Use C(group/community.dns.hosttech) in C(module_defaults) to set defaults for this module.
@@ -41,6 +44,7 @@ attributes:
 '''
 
     CONN = r'''
+options: {}
 attributes:
     become:
       description: Is usable alongside C(become) keywords.
@@ -51,18 +55,21 @@ attributes:
 '''
 
     FACTS = r'''
+options: {}
 attributes:
     facts:
       description: Action returns an C(ansible_facts) dictionary that will update existing host facts.
 '''
 
     FILES = r'''
+options: {}
 attributes:
     safe_file_operations:
       description: Uses Ansbile's strict file operation functions to ensure proper permissions and avoid data corruption.
 '''
 
     FLOW = r'''
+options: {}
 attributes:
     action:
       description: Indicates this has a corresponding action plugin so some parts of the options can be executed on the controller.
