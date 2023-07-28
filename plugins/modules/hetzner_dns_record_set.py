8d958ac58fe71d5fc5248bfaf8ad20291ce2418d
gpkvt <gpkvt@users.noreply.github.com>
2023-07-28 10:45:42 +0200

    Fixed CAA example (#153)
    * Fixed CAA example
    
    `ttl: 3600` isn't allowed by Hetzner and will result in `422 Unprocessable Entity: format: invalid CAA string;`, using `null` will use Hetzners default value, so it should always work.
    
    Also added quotes to the values.
    
    * Update plugins/modules/hetzner_dns_record_set.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/hetzner_dns_record_set.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update hetzner_dns_record_set.py
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/hetzner_dns_record_set.py
+++ plugins/modules/hetzner_dns_record_set.py
@@ -129,10 +129,9 @@ EXAMPLES = '''
     zone: foo.com
     record: foo.com
     type: CAA
-    ttl: 3600
     value:
-    - "128 issue letsencrypt.org"
-    - "128 iodef mailto:webmaster@foo.com"
+    - '128 issue "letsencrypt.org"'
+    - '128 iodef "mailto:webmaster@foo.com"'
     hetzner_token: access_token
 
 - name: Add an MX record
