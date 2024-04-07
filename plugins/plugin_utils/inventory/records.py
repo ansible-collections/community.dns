011e915774404c534edd1bc9724f0b0e79e966e0
Felix Fontein <felix@fontein.de>
2024-04-07 14:37:17 +0200

    Remove alias filters of option simple_filters.

--- plugins/plugin_utils/inventory/records.py
+++ plugins/plugin_utils/inventory/records.py
@@ -67,15 +67,7 @@ class RecordsInventoryModule(BaseInventoryPlugin):
     def parse(self, inventory, loader, path, cache=False):
         super(RecordsInventoryModule, self).parse(inventory, loader, path, cache)
 
-        orig_config = self._read_config_data(path)
-
-        if 'filters' in orig_config:
-            display.deprecated(
-                'The `filters` option of the %s inventory plugin has been renamed to `simple_filters`. '
-                'The old name will stop working in community.dns 3.0.0.' % self.NAME,
-                collection_name='community.dns',
-                version='3.0.0',
-            )
+        self._read_config_data(path)
 
         self.templar = Templar(loader=loader)
 
